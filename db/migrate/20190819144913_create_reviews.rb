338fe7483e894607ee5894bb4356a94cbf61c686
Felix Letellier <felix.letellier.merida@gmail.com>
2019-08-19 17:06:34 +0200

    Review model

---
+++ db/migrate/20190819144913_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.string :content
+      t.numeric :rating
+      t.references :user, foreign_key: true
+      t.references :apero, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

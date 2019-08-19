feb97987d51195bcd0f504a5a15dec9bc832f7cc
Edouard Blondeau <edouardbl@gmail.com>
2019-08-19 15:39:18 +0200

    added the user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :last_name, :first_name, :password, presence: true
+  validates :email, presence: true, uniqueness: true
+end
---
+++ db/migrate/20190819132849_add_columns_to_user.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :location, :string
+    add_column :users, :drinks, :string
+  end
+end

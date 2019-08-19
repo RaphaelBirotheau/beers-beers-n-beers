c4fa4bff3a6b234f84dcc57d0fdd369377c7cb99
thomas bozzo <thomas.bozzo@me.com>
2019-08-19 17:32:26 +0200

    debbuged routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- resources: aperos
+ resources :aperos
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

329e707616b422461b4e895f5caf3f64c174cbff
thomas bozzo <thomas.bozzo@me.com>
2019-08-19 16:50:07 +0200

    models aperos and bookings generated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :bookings
+  # has_many :booked_aperos, through: :bookings, class_name: 'Apero'
+  has_many :booked_aperos, :through => :bookings, :source => :apero
+  has_many :aperos
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :last_name, :first_name, :password, presence: true

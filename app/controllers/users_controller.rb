2bf43359b9bcf2a3b0212cc3a7934e8e17e45bea
Felix Letellier <felix.letellier.merida@gmail.com>
2019-08-20 14:33:38 +0200

    controller apero

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,31 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.all
-  end
+  # def index
+  #   @users = User.all
+  # end
 
-  def show
-    @user = User.find(params[:id])
-    # @dose = Dose.new
-  end
+  # def show
+  #   @user = User.find(params[:id])
+  #   # @dose = Dose.new
+  # end
 
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
-  def create
-    @user = User.new(user_params)
-    @user.save
-    redirect_to user_path(@user)
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   @user.save
+  #   redirect_to user_path(@user)
+  # end
 
-  private
+  # private
 
-  def user_params
-    params.require(:user).permit(:email, :first_name, :last_name, :password, :location, :drinks)
-  end
+  # def user_params
+  #   params.require(:user).permit(:email, :first_name, :last_name, :password, :location, :drinks)
+  # end
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 end
--- app/models/apero.rb
+++ app/models/apero.rb
@@ -2,5 +2,5 @@ class Apero < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  AMBIANCE = ["Ricard", "Dégustation", "Match de foot", "290 Bacth Mode", "YOLO"]
+  AMBIANCES = ["Ricard", "Dégustation", "Match de foot", "290 Bacth Mode", "YOLO"]
 end

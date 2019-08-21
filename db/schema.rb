c4394c678fc0cb8cd8acc81a8347dfbf08d1df3c
thomas bozzo <thomas.bozzo@me.com>
2019-08-21 16:00:59 +0200

    old schema collapse

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_08_21_132528)
+ActiveRecord::Schema.define(version: 2019_08_21_132528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +32,8 @@ ActiveRecord::Schema.define(version: 2019_08_21_132528)
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.bigint "apero_id"
     t.bigint "user_id"
+    t.bigint "apero_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["apero_id"], name: "index_bookings_on_apero_id"

919f7ee016cbedc80ddd3d5ed46f5b47b05eb9da
Felix Letellier <felix.letellier.merida@gmail.com>
2019-08-21 15:11:36 +0200

    add user avatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-usertest = User.create(first_name: "Paulo", last_name: "Lalcoolo", location:"Parisieeen", drinks:"Ricardo", email:"paulo.lalcoolo@ricardo.dirnk", password:"password")
-userorganizer = User.create(first_name: "Thomas", last_name: "bozzo", location:"Péguuuuu", drinks:"Pinard", email:"Thomas.pinard@bozzo.drink", password:"jemappelle")
-userdrinker =User.create(first_name: "Felix", last_name: "LM", location:"Cul-terreux", drinks:"sex on the beach", email:"felix.culterreux@sotb.drink", password:"lolita")
-aperotest = Apero.create!(date: "25 août", description: "allez viens, tu verras tout ce qu'on peut faire", place: "dtc", user: usertest, capacity: 12)
+
+require 'faker'
+
+10.times do
+  user = User.new(
+    first_name: Faker::JapaneseMedia::DragonBall.character,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    drinks: Faker::Games::Pokemon.name,
+    password: "password",
+  )
+  user.save!
+end

d90f93818d41dcfda8ddc479764066844957c5d0
raphael birotheau <raphael.birotheau@gmail.com>
2019-08-21 15:38:03 +0200

    home màj

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_21_085402) do
+ActiveRecord::Schema.define(version: 2019_08_21_083633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 2019_08_21_085402) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.bigint "user_id"
     t.bigint "apero_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["apero_id"], name: "index_bookings_on_apero_id"

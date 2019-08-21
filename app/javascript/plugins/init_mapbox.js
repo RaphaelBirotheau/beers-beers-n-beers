9bd5e6554a91e178263bf37aee76169b859231ea
raphael birotheau <raphael.birotheau@gmail.com>
2019-08-21 12:51:50 +0200

    updated markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -12,8 +12,14 @@ const buildMap = () => {
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
+  const element = document.createElement('div');
+  element.className = 'marker';
+  element.style.backgroundImage = `url('https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Emoji_u1f37a.svg/1024px-Emoji_u1f37a.svg.png')`;
+  element.style.backgroundSize = 'contain';
+  element.style.width = '40px';
+  element.style.height = '40px';
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
-    new mapboxgl.Marker()
+    new mapboxgl.Marker(element)
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup)
       .addTo(map)
@@ -34,5 +40,18 @@ const initMapbox = () => {
     fitMapToMarkers(map, markers);
   }
 };
+
+
+//   const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+//   // Create a HTML element for your custom marker
+
+//   // Pass the element as an argument to the new marker
+//   new mapboxgl.Marker(element)
+//     .setLngLat([marker.lng, marker.lat])
+//     .setPopup(popup)
+//     .addTo(map);
+// });
+
 
 export { initMapbox };

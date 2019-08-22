2613fb1c61dcb3c328aa098b479e62859e7b671e
raphael birotheau <raphael.birotheau@gmail.com>
2019-08-22 11:41:55 +0200

    added real searchbar

--- app/models/apero.rb
+++ app/models/apero.rb
@@ -3,7 +3,17 @@ class Apero < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_place?
   belongs_to :user
   has_many :bookings
-
+  has_many :users, through: :bookings
   mount_uploader :photo, PhotoUploader
   AMBIANCES = ["Ricard", "Binouse", "Dégustation", "Match de foot", "290 Batch Mode", "YOLO", "Vomito"]
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [:title, :place, :ambiance],
+    associated_against: {
+      user: [:first_name]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
